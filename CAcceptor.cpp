37d7172bfaea846eeb4ed89cbe46e800c57ab942
Hans-Christoph Steiner <hans@eds.org>
2011-12-05 18:49:51 -0500

    added most of the get/set properties

--- CAcceptor.cpp
+++ CAcceptor.cpp
@@ -12,7 +12,27 @@ BOOST_PYTHON_MODULE(mpost)
         .def("close", &CAcceptor::Close)
         .def("calibrate", &CAcceptor::Calibrate)
         .def("softreset", &CAcceptor::SoftReset)
+        .def("clearcashboxtotal", &CAcceptor::ClearCashBoxTotal)
         .def("escrowreturn", &CAcceptor::EscrowReturn)
         .def("escrowstack", &CAcceptor::EscrowStack)
+        .def("getbill", &CAcceptor::GetBill)
+        .add_property("autostack",
+                      &CAcceptor::GetAutoStack, &CAcceptor::SetAutoStack)
+        .add_property("acceptbills",
+                      &CAcceptor::GetEnableAcceptance, &CAcceptor::SetEnableAcceptance)
+        .add_property("highsecurity",
+                      &CAcceptor::GetHighSecurity, &CAcceptor::SetHighSecurity)
+        .add_property("debuglog",
+                      &CAcceptor::GetDebugLog, &CAcceptor::SetDebugLog)
+        .add_property("debuglogpath",
+                      &CAcceptor::GetDebugLogPath, &CAcceptor::SetDebugLogPath)
+        .add_property("billtypeenables",
+                      &CAcceptor::GetBillTypeEnables, &CAcceptor::SetBillTypeEnables)
+        .add_property("billvalueenables",
+                      &CAcceptor::GetBillValueEnables, &CAcceptor::SetBillValueEnables)
+/*
+        .add_property("",
+                      &CAcceptor::Get, &CAcceptor::Set)
+*/
         ;
 }
