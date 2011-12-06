1050b8e3a2f3c118ec8a5fb300b02d483410b87b
Hans-Christoph Steiner <hans@eds.org>
2011-12-06 13:44:15 -0500

    got bill tables returning as py lists

--- CAcceptor.cpp
+++ CAcceptor.cpp
@@ -1,17 +1,62 @@
 #include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include "Acceptor.h"
 
 using namespace boost::python;
 using namespace MPOST;
 
 
+struct CBill_to_python_tuple
+{
+    static PyObject* convert(CBill s)
+        {
+            boost::python::list* l = new boost::python::list();
+            (*l).append(s.GetCountry());
+            (*l).append((int)s.GetValue());
+            (*l).append(s.GetSeries());
+            (*l).append(s.GetType());
+            (*l).append(s.GetCompatibility());
+            (*l).append(s.GetVersion());
+            return l->ptr();
+        }
+};
+
+template<class T>
+struct VecToList
+{
+    static PyObject* convert(const std::vector<T>& vec)
+    {
+        boost::python::list* l = new boost::python::list();
+        for(size_t i = 0; i < vec.size(); i++)
+            (*l).append(vec[i]);
+
+        return l->ptr();
+    }
+};
+
 BOOST_PYTHON_MODULE(mpost)
 {
+    enum_<BNFStatus>("bnfstatus")
+        .value("unknown", Unknown)
+        .value("ok", OK)
+        .value("notattached", NotAttached)
+        .value("error", Error)
+        ;
+
     enum_<PowerUp>("powerup")
         .value("A", A)
         .value("B", B)
         .value("C", C)
         .value("E", E)
+        ;
+
+    enum_<PupExt>("pupext")
+        .value("return", Return)
+        .value("outofservice", OutOfService)
+        .value("stacknocredit", StackNoCredit)
+        .value("stack", Stack)
+        .value("waitnocredit", WaitNoCredit)
+        .value("wait", Wait)
         ;
 
     enum_<State>("state")
@@ -33,6 +78,12 @@ BOOST_PYTHON_MODULE(mpost)
         .value("downloadstart", DownloadStart)
         .value("downloadrestart", DownloadRestart)
         .value("downloading", Downloading)
+        ;
+
+    enum_<Bezel>("bezel")
+        .value("standard", Standard)
+        .value("platform", Platform)
+        .value("diagnostic", Diagnostic)
         ;
 
     enum_<Event>("event")
@@ -66,15 +117,57 @@ BOOST_PYTHON_MODULE(mpost)
         .value("Events_End", Events_End)
         ;
 
+    class_<CBill,boost::noncopyable>("CBill", "Represents a bill type")
+        .def("__repr__", &CBill::ToString)
+        .add_property("country", &CBill::GetCountry)
+        .add_property("value", &CBill::GetValue)
+        .add_property("type", &CBill::GetType)
+        .add_property("series", &CBill::GetSeries)
+        .add_property("compatibility", &CBill::GetCompatibility)
+        .add_property("version", &CBill::GetVersion)
+        ;
+
+    boost::python::to_python_converter<CBill, CBill_to_python_tuple>();
+    to_python_converter<std::vector<CBill, std::allocator<CBill> >, 
+                        VecToList<CBill> >();
+
     class_<CAcceptor,boost::noncopyable>("CAcceptor")
         .def("open", &CAcceptor::Open)
         .def("close", &CAcceptor::Close)
         .def("calibrate", &CAcceptor::Calibrate)
-        .def("softreset", &CAcceptor::SoftReset)
-        .def("clearcashboxtotal", &CAcceptor::ClearCashBoxTotal)
         .def("escrowreturn", &CAcceptor::EscrowReturn)
         .def("escrowstack", &CAcceptor::EscrowStack)
-        .def("getbill", &CAcceptor::GetBill)
+        .def("flashdownload", &CAcceptor::FlashDownload)
+        .def("softreset", &CAcceptor::SoftReset)
+        .def("clearcashboxtotal", &CAcceptor::ClearCashBoxTotal)
+        .add_property("bill", &CAcceptor::GetBill)
+        .add_property("billtypes", &CAcceptor::GetBillTypes)
+        .add_property("billvalues", &CAcceptor::GetBillValues)
+        .add_property("bnfstatus", &CAcceptor::GetBNFStatus)
+        .add_property("cashboxattached", &CAcceptor::GetCashBoxAttached)
+        .add_property("cashboxfull", &CAcceptor::GetCashBoxFull)
+        .add_property("cashboxtotal", &CAcceptor::GetCashBoxTotal)
+        .add_property("connected", &CAcceptor::GetConnected)
+        .add_property("devicebusy", &CAcceptor::GetDeviceBusy)
+        .add_property("devicecrc", &CAcceptor::GetDeviceCRC)
+        .add_property("devicefailure", &CAcceptor::GetDeviceFailure)
+        .add_property("devicejammed", &CAcceptor::GetDeviceJammed)
+        .add_property("devicemodel", &CAcceptor::GetDeviceModel)
+        .add_property("devicepaused", &CAcceptor::GetDevicePaused)
+        .add_property("deviceportname", &CAcceptor::GetDevicePortName)
+        .add_property("devicepowerup", &CAcceptor::GetDevicePowerUp)
+        .add_property("deviceresets", &CAcceptor::GetDeviceResets)
+        .add_property("devicerevision", &CAcceptor::GetDeviceRevision)
+        .add_property("deviceserialnumber", &CAcceptor::GetDeviceSerialNumber)
+        .add_property("devicestalled", &CAcceptor::GetDeviceStalled)
+        .add_property("devicestate", &CAcceptor::GetDeviceState)
+        .add_property("devicetype", &CAcceptor::GetDeviceType)
+/*
+        .add_property("", &CAcceptor::Get)
+        .add_property("", &CAcceptor::Get)
+        .add_property("", &CAcceptor::Get)
+        .add_property("", &CAcceptor::Get)
+*/
         .add_property("autostack",
                       &CAcceptor::GetAutoStack, &CAcceptor::SetAutoStack)
         .add_property("acceptbills",
--- mpost_test.py
+++ mpost_test.py
@@ -1,13 +1,40 @@
 #!/usr/bin/python
 
 import time
+import glob
 import mpost
 
-m = mpost.CAcceptor()
-m.open("/dev/loc-bill-ttyUSB0", mpost.powerup.A)
+acceptors = []
 
-m.acceptbills = True
-
-time.sleep(10)
-
-m.close()
+for f in glob.glob('/dev/loc-bill-ttyUSB*'):
+    m = mpost.CAcceptor()
+    m.open(f, mpost.powerup.A)
+    # sleep after opening since it can take a while to get
+    time.sleep(5)
+    print 'm.devicestate: ',
+    print m.devicestate
+    m.autostack = True
+    m.acceptbills = True
+    acceptors.append(m)
+    print "device: "
+    print m.deviceportname
+    print m.devicemodel
+#    print m.devicetype
+    print m.devicerevision
+    print 'm.bill: ',
+    print m.bill
+    print 'm.billtypes: ',
+    print m.billtypes
+    print 'm.billvalues: '
+    setbillvalues = []
+    for bill in m.billvalues:
+        if bill[0] != 'USD' or bill[0] != 'RUB':
+            setbillvalues.append(bill)
+            print bill[0]
+        else:
+            print 'NOT: ',
+            print bill
+    m.autostack = False
+    m.acceptbills = False
+    time.sleep(6)
+    m.close()
